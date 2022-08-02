574699a5d3a53fa9e790de413f14ec42315c2e08
ngxson <thichthat@gmail.com>
2022-08-02 11:40:14 +0200

    add systemd unit

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+# install binary
 npm run build
 sudo cp frmw-service /usr/sbin/frmw-service
 sudo chown root:root /usr/sbin/frmw-service
 sudo chmod 744 /usr/sbin/frmw-service
+
+# install systemd unit
+sudo cp ./systemd/frmw.service /etc/systemd/system/frmw.service
+sudo chmod 644 /etc/systemd/system/frmw.service
+sudo chown root:root /etc/systemd/system/frmw.service
+sudo systemctl --daemon-reload
+sudo systemctl enable frmw.service
+sudo systemctl start frmw.service
