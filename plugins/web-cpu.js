b7f0674a8541959f133f43f74f173f9ba2dfa118
ngxson <thichthat@gmail.com>
2022-08-05 15:26:38 +0200

    small fix

--- plugins/web-cpu.js
+++ plugins/web-cpu.js
@@ -10,7 +10,9 @@ const getHTMLContent = () => {
   const { name, powerLimitsBattery, powerLimitsAC, autoPowerLimit, autoCoreLimit, pCores } = cpu.getCPUModelConfig();
   const currPL1 = readFloat('/sys/class/powercap/intel-rapl:0/constraint_0_power_limit_uw') / 1000000;
   const currPL2 = readFloat('/sys/class/powercap/intel-rapl:0/constraint_1_power_limit_uw') / 1000000;
-  const enableCores = fs.readFileSync('/sys/fs/cgroup/cpuset/active_cores/cpuset.cpus').toString();
+  const enableCores = pCores
+    ? fs.readFileSync('/sys/fs/cgroup/cpuset/active_cores/cpuset.cpus').toString().replace('-', ' to cpu')
+    : 'All cores';
   return `
     <br/>
     This module let you switch PL1, PL2, enabled cores values on AC/battery mode. On battery, all processes will be moved to E-cores for better power efficient.<br/>
@@ -19,7 +21,7 @@ const getHTMLContent = () => {
     <b>CPU model:</b> ${name}<br/>
     <b>Current PL1:</b> ${Math.round(currPL1)} W<br/>
     <b>Current PL2:</b> ${Math.round(currPL2)} W<br/>
-    <b>Enabled cores:</b> from cpu${enableCores.replace('-', ' to cpu')}<br/>
+    <b>Enabled cores:</b> from cpu${enableCores}<br/>
     <b>Auto change:</b> ${autoPowerLimit ? '<span style="color: green">ON</span>' : '<span style="color: gray">OFF</span>'}<br/>
     <button onclick="window.location.reload()">Reload</button><br/>
     <br/>
